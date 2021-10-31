79dfc7a01ee7450c9b11a0df29aa83cf8a96c2e6
jessicaSoares <jessicassoarez@gmail.com>
2021-10-31 11:08:04 -0300

    navbar2

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,20 +8,7 @@ function Cards() {
       <h1>Indicadores mais acessados</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-3.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul>
+
           <ul className='cards__items'>
             <CardItem
               src='images/img-3.jpg'
