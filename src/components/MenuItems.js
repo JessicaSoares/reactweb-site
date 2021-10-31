2f3ad6d00da3e3a7e8fe00b51af844e84b5c7fe6
jessicaSoares <jessicassoarez@gmail.com>
2021-10-31 16:02:21 -0300

    naavbar part 4

---
+++ src/components/MenuItems.js
@@ -0,0 +1,62 @@
+export const MenuItems = [
+  {
+    title: 'Agronegócio',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Paineis Orçamentários',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Assistencia social',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Desenvolvimento humano',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Economia',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Educação',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Empregos',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Infraestrutura',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Mapas dinâmicos',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Meio Ambiente',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Saúde',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Trânsito',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+];
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Dropdown from './Dropdown';
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -22,12 +32,21 @@ function Navbar() {
     showButton();
   }, []);
 
+
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className='navbar'>
-        <div className='navbar-container'>
           <p className='navbar-logo' >
           <img src='images/logo.png'/> 
           Observatório de Dados Estatísticos <br/> e Geografícos de Parauapebas
@@ -36,52 +55,56 @@ function Navbar() {
 
 
           <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Inicio
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/services'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Indicadores
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Mapas Dinâmicos
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Perfil do município
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Observatório
-              </Link>
-            </li>
-          </ul>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Início
+            </Link>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to='/services'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Indicadores <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/products'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Mapas dinâmicos
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/contact-us'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Perfil do município
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/contact-us'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+             Observatório
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   );
