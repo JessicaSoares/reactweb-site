04ed0666829fb3aa564f910d1c59c3196391baf5
jessicaSoares <jessicassoarez@gmail.com>
2021-11-21 23:35:40 -0300

    test

--- src/components/Aboutother.js
+++ src/components/Aboutother.js
@@ -27,34 +27,34 @@ function Aboutother() {
                         <div class="mt-5">
                             <div class="mb-2"><i class="bi-gem fs-1 text-primary"> <CgSearch color='#0f295a' /></i></div>
                             <h3 class="h4 mb-2">O que encontra</h3>
-                            <p class="text-muted mb-0"><p>•  Dashboards, gráficos, infográficos e mapas;</p>
-<p>•  Relatórios estatísticos;</p>
-<p>•  Indicadores de desempenho;</p>
-<p>•  Georreferenciamento de Informações;</p>
-<p>•  Transparências de ações e investimentos</p></p>
+                            <ol class="text-muted mb-0"><li>•  Dashboards, gráficos, infográficos e mapas;</li>
+<li>•  Relatórios estatísticos;</li>
+<li>•  Indicadores de desempenho;</li>
+<li>•  Georreferenciamento de Informações;</li>
+<li>•  Transparências de ações e investimentos</li></ol>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 text-center">
                         <div class="mt-5">
                             <div class="mb-2"><i class="bi-laptop fs-1 text-primary"><BsGraphUp color='#0f295a' /></i></div>
                             <h3 class="h4 mb-2">Tipos de indicadores</h3>
-                            <p class="text-muted mb-0"><p>•  Sociais: População, trabalho, educação, saúde, habitação.</p>
-<p></p>
-<p>• Econômicos: agricultura, pecuária, indústria, construção, comércio;</p>
-<p></p>
-<p>• Outros: Gênero, cultura, recreação e esporte, turismo, meio ambiente.</p></p>
+                            <ol class="text-muted mb-0"><li>•  <b>Sociais</b>: População, trabalho, educação;</li>
+
+<li>• <b>Econômicos</b>: agricultura, pecuária, indústria, construção, comércio;</li>
+
+<li>• <b>Outros</b>: Gênero, cultura, recreação e esporte, turismo, meio ambiente.</li></ol>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 text-center">
                         <div class="mt-5">
                             <div class="mb-2"><i class="bi-globe fs-1 text-primary"><BsPeopleFill color='#0f295a' /></i></div>
                             <h3 class="h4 mb-2">A que se destina?</h3>
-                            <p class="text-muted mb-0">•  Gestores públicos para monitoramento e tomada de decisão;
-                            <p>• Dashboards, gráficos, infográficos e mapas; </p>
-<p>• Relatórios estatísticos; </p>
-<p>• Indicadores de desempenho;</p>
-<p>• Georreferenciamento de Informações;</p>
-</p>
+                            <ol class="text-muted mb-0"><li class="text-muted mb-0">• Gestores públicos para monitoramento e tomada de decisão; </li>
+                          <li> • Dashboards, gráficos, infográficos e mapas;  </li>
+                          <li> • Relatórios estatísticos; </li>
+                          <li> • Indicadores de desempenho;</li>
+                          <li> • Georreferenciamento de Informações. </li></ol>
+
                         </div>
                     </div>
 
